fcbdb2b544dc531545c78a02e82f48133c4aacb3
Chris Tralie <chris.tralie@gmail.com>
2020-02-03 20:55:51 -0500

    Adding unit test skeleton code

---
+++ test/hw2/HW2Test.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hw2;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class HW2Test {
+    
+    public HW2Test() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    /**
+     * Test of high income
+     */
+    @Test
+    public void testHighIncome() {
+        double income = 45000;
+        int numKids = 5;
+        double expResult = 0.0; // The expected result
+        double result = HW2.computeAssistance(income, numKids);
+        assertEquals(expResult, result, 0.0);
+    }
+    
+    @Test
+    public void testMidIncome1() {
+        double income = 35000;
+        int numKids = 3;
+        double expResult = 3*1000.0; // The expected result
+        double result = HW2.computeAssistance(income, numKids);
+        assertEquals(expResult, result, 0.0);
+    }
+    
+}
